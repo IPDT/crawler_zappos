94be45fb251d4940521bb335b49d7792fb02efc1
gbzhu <gbzhu@zju.edu.cn>
2017-12-28 14:50:48 +0800

    download video and db backup

--- zappos_v2/crawler_1st.py
+++ zappos_v2/crawler_1st.py
@@ -9,13 +9,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 # for mac
-# browserPath = '/Users/gbzhu/software/phantomjs-2.1.1-macosx/bin/phantomjs'  # 浏览器的地址
+browserPath = '/Users/gbzhu/software/phantomjs-2.1.1-macosx/bin/phantomjs'  # 浏览器的地址
 
 # for windows
 # browserPath = 'C:\\Program Files (x86)\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe'  # 浏览器的地址
 
 # for linux
-browserPath = '/usr/lib/phantomjs/phantomjs'
+# browserPath = '/usr/lib/phantomjs/phantomjs'
 
 driver = webdriver.PhantomJS(executable_path=browserPath)  # 加载浏览器
 parser = 'html5lib'  # 解析器
@@ -35,16 +35,16 @@ all_tag_list = ['Brand']
 # db information
 
 # for mac
-# db = pymysql.connect(host="localhost", user="root", password="gbzhuroot", database="zappos_v2", use_unicode=True,
-#                      charset='utf8')
+db = pymysql.connect(host="localhost", user="root", password="gbzhuroot", database="zappos_v2", use_unicode=True,
+                     charset='utf8')
 
 # for windows
 # db = pymysql.connect(host="localhost", user="root", passwd="root", db="zappos_v2", use_unicode=True,
 #                      charset='utf8')
 
 # for linux
-db = pymysql.connect(host="localhost", user="root", passwd="shenqinglengmo", db="zappos_v2", use_unicode=True,
-                     charset='utf8')
+# db = pymysql.connect(host="localhost", user="root", passwd="shenqinglengmo", db="zappos_v2", use_unicode=True,
+#                      charset='utf8')
 
 cursor = db.cursor()
 
---
+++ zappos_v2/download_video.py
@@ -0,0 +1,62 @@
+"""
+    download video from url
+"""
+import os
+from urllib import request
+from urllib.error import ContentTooShortError
+from urllib.error import HTTPError
+
+from zappos_v2.crawler_1st import cursor, db, id_info_table
+
+
+def download_video(db_row: tuple):
+    pre_url = 'https://www.zappos.com/media/video/replace_chars.mp4'
+    style_id, product_id, category, brand = str(db_row[0]), str(db_row[1]), db_row[2], db_row[3]
+
+    print(category + ' - ' + brand + ' - ' + style_id)
+
+    replace_chars = product_id[0] + '/' + product_id[1] + '/' + product_id[2] + '/' + product_id
+    # for mac
+    output_dir = '/Users/gbzhu/data/sap_data/picture_v2/' + category + '/' + brand + '/' + 'video' + '/'
+
+    # for windows
+    # output_dir = 'C://Users//I342202//Desktop//picture_v2//' + category + '//' + brand + '//' + 'video' + '//'
+
+    # linux
+    # output_dir = '/home/I342202/picture_v2/' + category + '/' + brand + '/' + 'video' + '/'
+
+    url = pre_url.replace('replace_chars', replace_chars)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    video_path = output_dir + style_id + '.mp4'
+    if os.path.exists(video_path):
+        print('exist')
+    else:
+        try:
+            request.urlretrieve(url, video_path)
+        except ContentTooShortError:
+            print('this video interrupted')
+            sql = "update " + id_info_table + " set video_tag=2 where style_id=" + style_id
+            cursor.execute(sql)
+            db.commit()
+            return None
+        except HTTPError:
+            print('url 404')
+            sql = "update " + id_info_table + " set video_tag=404 where style_id=" + style_id
+            cursor.execute(sql)
+            db.commit()
+            return None
+        sql = "update " + id_info_table + " set video_tag=1 where style_id=" + style_id
+        cursor.execute(sql)
+        db.commit()
+
+
+if __name__ == '__main__':
+    sql = "select style_id,product_id,category ,brand from " + id_info_table + " where isdownload=1"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for row in rows:
+        download_video(db_row=row)
+    cursor.close()
+    db.close()
